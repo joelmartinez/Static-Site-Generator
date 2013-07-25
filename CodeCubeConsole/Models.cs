982074ebae3434327ad719fbef384f26fd88832b
Joel Martinez <joelmartinez@gmail.com>
2013-07-24 21:50:26 -0400

    Added master html5 template, and moved models into a separate file

---
+++ CodeCubeConsole/Models.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CodeCubeConsole
+{
+    public class Year
+    {
+        public string Name { get; set; }
+        public Month[] Months { get; set; }
+    }
+    public class Month
+    {
+        public string Name { get; set; }
+        public Post[] Posts { get; set; }
+    }
+    public class Post
+    {
+
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string ParsedBody
+        {
+            get
+            {
+                string body = this.Body;
+                body = body.Replace("\n\n", "<br /><br />");
+
+                if (body.Contains("[gist"))
+                {
+                    body = Regex.Replace(body, "\\[gist id=([0-9]+)\\]", "<script src=\"https://gist.github.com/joelmartinez/$1.js\"></script>");
+                }
+                return body;
+            }
+        }
+        public string URL { get; set; }
+        public string UrlPath
+        {
+            get
+            {
+                return new Uri(this.URL).AbsolutePath;
+            }
+        }
+        public DateTime PublishedOn { get; set; }
+
+    }
+}
--- CodeCubeConsole/Program.cs
+++ CodeCubeConsole/Program.cs
@@ -3,54 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace CodeCubeConsole
 {
-    public class Year
-    {
-        public string Name { get; set; }
-        public Month[] Months { get; set; }
-    }
-    public class Month
-    {
-        public string Name { get; set; }
-        public Post[] Posts { get; set; }
-    }
-    public class Post
-    {
-
-        public string Title { get; set; }
-        public string Body { get; set; }
-        public string ParsedBody
-        {
-            get
-            {
-                string body = this.Body;
-                body = body.Replace("\n\n", "<br /><br />");
-                
-                if (body.Contains("[gist"))
-                {
-                    body = Regex.Replace(body, "\\[gist id=([0-9]+)\\]", "<script src=\"https://gist.github.com/joelmartinez/$1.js\"></script>");
-                }
-                return body;
-            }
-        }
-        public string URL { get; set; }
-        public string UrlPath
-        {
-            get
-            {
-                return new Uri(this.URL).AbsolutePath;
-            }
-        }
-        public DateTime PublishedOn { get; set; }
-
-    }
-    
     class Program
     {
         static void Main(string[] args)
@@ -61,8 +18,6 @@ namespace CodeCubeConsole
 
         private static async void BuildSite()
         {
-            //Directory.Delete("out", true);
-
             var model = GetContent();
 
             // start with the index page
